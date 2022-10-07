59f94468f9a861757a953952953b1307b1a9ff0c
Matheus Ferreira <jeronimomatheus05@gmail.com>
2022-10-07 18:55:25 -0300

    primeiras adições no Javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+const directions = document.querySelectorAll(".divDirection i");
+const color01 = document.querySelector("#color01");
+const color02 = document.querySelector("#color02");
+const btnGerar = document.querySelector("#button");
+const output = document.querySelector("textarea");
+let direction = "to top";
